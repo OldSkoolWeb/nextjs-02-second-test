817c038e3cd9207c56889adf4849cddeacdcc2e2
OldSkoolWeb <oldskoolweb@mail.com>
2023-10-12 23:04:28 +0100

    Simple Scss setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import variables from './variables.module.scss'
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      
+<h1 style={{ color: variables.primaryColor }}>Hello, Next.js!</h1>
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
